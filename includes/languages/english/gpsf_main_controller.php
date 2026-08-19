3b29f855f9171d8e033ef63de17577d51faef5e5
Melanie Prough <mprough@pro-webs.net>
2026-08-19 10:05:41 -0400

    Rebrand and separate reimagined release version

--- includes/languages/english/gpsf_main_controller.php
+++ includes/languages/english/gpsf_main_controller.php
@@ -1,10 +1,10 @@
 <?php
 // -----
-// Google Product Search Feeder II, main controller language file.
+// Red Headed Stepchild of Zen Cart® Google Product Search Feeder II, main controller language file.
 // Copyright 2023-2026, https://vinosdefrutastropicales.com
 // Modifications Copyright 2026 PRO-Webs, Inc. (Melanie Prough), https://PRO-Webs.net
 //
-// Last updated: v1.0.9
+// Last updated: Reimagined Release v1.0.3
 //
 /**
  * Based on:
@@ -16,7 +16,7 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: google_product_search_defines.php 5 2011-11-17 11:19:31Z numinix $
  */
-define('TEXT_GPSF_STARTED', 'Google Product Search Feeder II v%s started ' . date('Y/m/d H:i:s'));
+define('TEXT_GPSF_STARTED', 'Red Headed Stepchild of Zen Cart® Google Product Search Feeder II (v1.0.5), Reimagined Release v%s started ' . date('Y/m/d H:i:s'));
 define('TEXT_GPSF_FILE_LOCATION', 'Feed file - ');
 
 //- %f is the number of seconds the feed took to process
