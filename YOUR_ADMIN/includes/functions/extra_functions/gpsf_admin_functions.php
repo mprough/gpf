fb3eb39bde0d65f5b3b8dfde155d3db49c329023
Melanie Prough <mprough@pro-webs.net>
2026-08-19 10:05:51 -0400

    Rebrand and separate reimagined release version

--- YOUR_ADMIN/includes/functions/extra_functions/gpsf_admin_functions.php
+++ YOUR_ADMIN/includes/functions/extra_functions/gpsf_admin_functions.php
@@ -1,10 +1,10 @@
 <?php
 // -----
-// Google Product Search Feeder II, admin tool.
+// Red Headed Stepchild of Zen Cart® Google Product Search Feeder II, admin tool.
 // Copyright 2023-2026, https://vinosdefrutastropicales.com
 // Modifications Copyright 2026 PRO-Webs, Inc. (Melanie Prough), https://PRO-Webs.net
 //
-// Last updated: v1.0.9
+// Last updated: Reimagined Release v1.0.3
 //
 /**
  * Based on:
