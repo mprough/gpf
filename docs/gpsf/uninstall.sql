e8ecb105025e730578a8eae71160f720855ca587
Melanie Prough <mprough@pro-webs.net>
2026-08-19 10:05:56 -0400

    Rebrand and separate reimagined release version

--- docs/gpsf/uninstall.sql
+++ docs/gpsf/uninstall.sql
@@ -1,3 +1,3 @@
 DELETE FROM configuration WHERE configuration_key LIKE 'GPSF_%';
-DELETE FROM configuration_group WHERE configuration_group_title = 'Google Product Search Feeder II';
+DELETE FROM configuration_group WHERE configuration_group_title = 'Red Headed Stepchild of Zen Cart® Google Product Search Feeder II';
 DELETE FROM admin_pages WHERE page_key IN('configGpsf', 'toolGpsf');
