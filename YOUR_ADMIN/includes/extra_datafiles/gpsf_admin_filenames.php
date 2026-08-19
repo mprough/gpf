a9636b85d758dd770cb798064c49adce9eb77206
Melanie Prough <mprough@pro-webs.net>
2026-08-19 10:05:45 -0400

    Rebrand and separate reimagined release version

--- YOUR_ADMIN/includes/extra_datafiles/gpsf_admin_filenames.php
+++ YOUR_ADMIN/includes/extra_datafiles/gpsf_admin_filenames.php
@@ -1,10 +1,10 @@
 <?php
 // -----
-// Part of the Google Product Search Feeder II
+// Part of the Red Headed Stepchild of Zen Cart® Google Product Search Feeder II
 // Copyright 2023-2026, https://vinosdefrutastropicales.com
 // Modifications Copyright 2026 PRO-Webs, Inc. (Melanie Prough), https://PRO-Webs.net
 //
-// Last updated: v1.0.9
+// Last updated: Reimagined Release v1.0.3
 //
 define('FILENAME_GPSF_ADMIN', 'gpsf_admin');
 define('FILENAME_GPSF_MAIN_CONTROLLER', 'gpsf_main_controller');
